8a20bcee02ddc740d7d0d0f4b0ab39d7ea85bec0
silvanalorens <silvanalorens@gmail.com>
2024-08-13 22:57:27 -0500

    crud commit

--- src/main/java/com/mitocode/springreactor/controller/DishController.java
+++ src/main/java/com/mitocode/springreactor/controller/DishController.java
@@ -5,9 +5,13 @@ import com.mitocode.springreactor.service.IDishService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.net.URI;
 
 @RestController
 //Controller y ResponseBody devuelve json
@@ -34,19 +38,39 @@ public class DishController {
                 .defaultIfEmpty(ResponseEntity.notFound().build());
     }
     @PostMapping
-    public Mono<ResponseEntity<Dish>> save(@RequestBody Dish dish){
+    public Mono<ResponseEntity<Dish>> save(@RequestBody Dish dish,
+                                           final ServerHttpRequest req){
         return service.save(dish)
-                .map(mx -> ResponseEntity.ok()
+                .map(e -> ResponseEntity.created(
+                        URI.create(req.getURI().toString().concat("/").concat(e.getId()))
+                        )
                         .contentType(MediaType.APPLICATION_JSON)
-                        .body(mx));
+                        .body(e))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
     @PutMapping("/{id}")
-    public Mono<Dish> update(@PathVariable("id") String id,@RequestBody Dish dish){
-        return service.update(id,dish);
+    public Mono<ResponseEntity<Dish>> update(@PathVariable("id") String id,@RequestBody Dish dish){
+        return Mono.just(dish)
+                .map(e->{
+                    e.setId(id);
+                    return e;
+                })
+                .flatMap(e->service.update(id,e))
+                .map(e->ResponseEntity.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(e)
+                ).defaultIfEmpty(ResponseEntity.notFound().build());
+
     }
     @DeleteMapping("/{id}")
-    public Mono<Boolean> delete(@PathVariable("id") String id){
-        return service.delete(id);
+    public Mono<ResponseEntity<Void>> delete(@PathVariable("id") String id){
+        return service.delete(id)
+                .flatMap(result->{
+                        if (result)
+                            return Mono.just(ResponseEntity.noContent().build());
+                        else
+                            return Mono.just(ResponseEntity.notFound().build());
+                });
     }
 
 }
